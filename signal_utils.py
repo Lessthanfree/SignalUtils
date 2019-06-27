Fixed Spectrogram Normalisation: Changed variance to std deviation

signal_utils.py
@@ -62,7 +62,6 @@ def lr_open_wav(filename, sample_rate, DEBUG=0):
 #     p_wav = np.average(p_frame_avg) # Average power of the entire audio wave
 #     return p
 
-
 # In: Waveform
 # Out: Spectrogram
 def conv_to_spectro(audio):
@@ -197,7 +196,29 @@ def get_context(i_spectro, hop=1, context=5):
         i+=hop
     return chunks
 
-#In: np array as Single Spectrogram
+#In: Single Transposed Spectro as np array
+#Out: Context window of specified frame
+def get_context_frame(t_spectro, context=5, frame_num):
+    frames = t_spectro.shape[0]        # Num frames
+    i = frame_num
+    # start padding required
+    if i < context:
+          num_pad = int(context - i)
+          pad = np.tile(t_spectro[0], (num_pad,1))        # Generate padding
+          back = np.append(pad,t_spectro[:i+1], axis = 0) # Back context + middle
+    else:
+        back = t_spectro[i-context:i+1]                 # Back context + middle
+    # end padding is required
+    if i + context > frames-1:
+        num_pad = i + context - frames + 1
+        pad = np.tile(t_spectro[frames-1], (num_pad,1)) # Generate padding
+        front = np.append(t_spectro[i+1:],pad, axis = 0) # Front context
+    else:
+        front = t_spectro[i+1:i+1+context] # Front context
+    context_win = np.append(back,front,axis=0)
+    return context_win
+
+#In: Single Transposed Spectrogram as np array
 #Out: np array of context windows w +- 5 frames with hop length 'hop'
 def get_context_spectro(t_spectro, context=5, hop=1):
   freqs = int(t_spectro.shape[1])    # Frequencies
@@ -249,8 +270,7 @@ def get_context_spectro(t_spectro, context=5, hop=1):
       #print("single",chunk.shape,"collection",chunks.shape)
       chunks = np.append(chunks,chunk,axis=0)
       i+=hop
-    return chunks
-    
+    return chunks    
 
 # In: Numpy array of spectrograms
 # Out: Numpy array of transposed spectrograms
@@ -270,20 +290,18 @@ def normalise_inputs(inputs):
     # Concatenate to 1 array
     inputs_concat = normalise_concat(inputs) 
     averages = []
-    variances = []
+    stds = []
     rows = inputs_concat.shape[0]
     
     averages = np.mean(inputs_concat, axis=1)      # Average across all bins
-    variances = np.var(inputs_concat, axis=1)   # Variance of all bins
-    print(averages)
-    print(variances)
+    stds = np.std(inputs_concat, axis=1)   # Variance of all bins
 
     # Normalize 
     for key in inputs.keys():
       for row in range(rows):
-        inputs[key][row] = (inputs[key][row]-averages[row])/variances[row]
+        inputs[key][row] = (inputs[key][row]-averages[row])/stds[row]
       
-    return inputs, inputs_concat, averages, variances
+    return inputs, inputs_concat, averages, stds
 
 #In: Dictionary of spectrograms
 #Out: Numpy arr of concatenated spectrograms